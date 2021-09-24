ed0d60ae4407097322c0a4044efce816ea18dd2a
iGuzmanF <201900215@ujmd.edu.sv>
2021-09-24 14:20:42 -0600

    Se agrego 2 Ejercicio Parcial

---
+++ src/edu/ujmd/Ejercicio/Clases/Persona.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.ujmd.Ejercicio.Clases;
+
+/**
+ *
+ * @author chele
+ */
+public class Persona {
+
+    public String Nombre;
+    public String Apellido;
+    public String CargoE;
+
+    public Persona() {
+
+    }
+
+    public Persona(String Nombre, String Apellido, String CargoE) {
+        this.Nombre = Nombre;
+        this.Apellido = Apellido;
+        this.CargoE = CargoE;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setApellido(String Apellido) {
+        this.Apellido = Apellido;
+    }
+
+    public String getApellido() {
+        return Apellido;
+    }
+
+    public void setCargoE(String Cargo) {
+        this.CargoE = Cargo;
+    }
+
+    public String getCargoE() {
+        return CargoE;
+    }
+}
